9335f894eb56d46a2a62f3369efadad7d453f1b4
CPMKurdish <kura1212king@gmail.com>
2025-02-03 15:49:08 +0000

    Update cpmkurdish.py

--- cpmkurdish.py
+++ cpmkurdish.py
@@ -148,7 +148,7 @@ class CPMKurdish:
     def set_player_wins(self, amount, amount1) -> bool:
         payload = {"account_auth": self.auth_token,
         "amount": amount,
-        "amount": amount1,
+        "amount1": amount1,
         }
         params = {"key": self.access_key}
         response = requests.post(
