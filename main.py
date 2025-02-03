1faadc2fbb5ab78f8a9a8ccfe0c4e6c78bd03a82
CPMKurdish <kura1212king@gmail.com>
2025-02-03 17:39:59 +0000

    Update main.py

--- main.py
+++ main.py
@@ -731,10 +731,10 @@ if __name__ == "__main__":
                 console.print("[red][%] HACKING CAR SPEED[/red]: ", end=None)
                 if cpm.hack_car_speed(car_id):
                     print(Colorate.Horizontal(Colors.rainbow, 'SUCCESSFUL'))
-                    answ = Prompt.ask("[red][?] DO YOU WANT TO EXIT[/red] ?", choices=["y", "n"], default="n")
+                         answ = Prompt.ask("[red][?] DO YOU WANT TO EXIT[/red] ?", choices=["y", "n"], default="n")
                         if answ == "y": print(Colorate.Horizontal(Colors.rainbow, f'THANK YOU FOR USING OUR TOOL, PLEASE JOIN OUR TELEGRAM CHANNEL: @{__CHANNEL_USERNAME__}'))
-                    else: continue
-                else:
+                        else: continue
+                    else:
                     print(Colorate.Horizontal(Colors.rainbow, 'FAILED'))
                     print(Colorate.Horizontal(Colors.rainbow, 'PLEASE TRY AGAIN'))
                     sleep(2)
