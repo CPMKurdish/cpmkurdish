f845389fc43af67ba8561a44fa1c38f91d272a5a
CPMKurdish <kura1212king@gmail.com>
2025-02-03 15:19:13 +0000

    Update cpmkurdish.py

--- cpmkurdish.py
+++ cpmkurdish.py
@@ -148,13 +148,15 @@ class CPMKurdish:
     def set_player_wins(self, amount, amount) -> bool:
         payload = {
         "account_auth": self.auth_token,
-        "amount": amount,
-        "amount": amount,        
+        "amount": amount
+        "amount": amount        
         }
         params = {"key": self.access_key}
-        response = requests.post(f"{BASE_URL}/set_race_wins", params=params, data=payload)
+        response = requests.post(
+            f"{BASE_URL}/set_race_wins", params=params, data=payload
+        )
         response_decoded = response.json()
-        return response_decoded.get("ok")        
+        return response_decoded.get("ok")
 
     def set_player_loses(self, amount) -> bool:
         payload = {"account_auth": self.auth_token, "amount": amount}
@@ -239,18 +241,14 @@ class CPMKurdish:
         response_decoded = response.json()
         return response_decoded.get("ok")
         
-    def hack_car_sexo(self, amount0, amount1, amount2, amount3) -> bool:
-        payload = {"account_auth": self.auth_token,
-        "amount0": amount0}
-        "amount1": amount1}
-        "amount2": amount2}
-        "amount3": amount3}                        
+    def hack_car_sexo(self) -> bool:
+        payload = {"account_auth": self.auth_token}
         params = {"key": self.access_key}
         response = requests.post(
             f"{BASE_URL}/hack_car_sexo", params=params, data=payload
         )
         response_decoded = response.json()
-        return response_decoded.get("ok")        
+        return response_decoded.get("ok")                
         
     def chrome_all_cars(self) -> bool:
         payload = {"account_auth": self.auth_token}
