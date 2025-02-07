7ed816f737a79e4f6abc03d3cbb3ea5eb7dffc0e
CPMKurdish <kura1212king@gmail.com>
2025-02-07 09:55:11 +0000

    Update cpmkurdish.py

--- cpmkurdish.py
+++ cpmkurdish.py
@@ -289,11 +289,15 @@ class CPMKurdish:
         response_decoded = response.json()
         return response_decoded.get("ok")             
         
-    def remove_car_bumper(self) -> bool:
-        payload = {"account_auth": self.auth_token}
+    def remove_car_bumper(self, car_idd):
+        payload = {
+        "account_auth": self.auth_token,
+         "car_idd": car_idd,
+         
+         }
         params = {"key": self.access_key}
         response = requests.post(
             f"{BASE_URL}/remove_car_bumper", params=params, data=payload
         )
         response_decoded = response.json()
-        return response_decoded.get("ok")                        
+        return response_decoded.get("ok")                                                                              
