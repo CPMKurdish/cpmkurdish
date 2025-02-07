edf1970502447769f4ed5f6940e81932c12c3229
CPMKurdish <kura1212king@gmail.com>
2025-02-07 20:43:32 +0000

    Update main.py

--- main.py
+++ main.py
@@ -907,6 +907,5 @@ if __name__ == "__main__":
                 print(Colorate.Horizontal(Colors.rainbow, '! NOTE: MAKE SURE YOU FILLED OUT THE FIELDS'))
                 sleep(2)
                 continue
-            else: continue
             break
         break
