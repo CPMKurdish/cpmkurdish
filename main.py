f1d17f4d3a96b84170b7788efa4ccd44058eff1f
CPMKurdish <kura1212king@gmail.com>
2025-02-09 23:33:08 +0000

    Update main.py

--- main.py
+++ main.py
@@ -224,7 +224,7 @@ def load_player_data(cpm):
         if 'floats' in data and 'localID' in data and 'money' in data and 'coin' in data and 'localIID' in data:
         
             
-            print(Colorate.Horizontal(Colors.rainbow, Center.XCenter(f'Name: {(data.get("Name") if "Name" in data else "UNDEFINED")} <> LocalID: {data.get("localID")} <> Money: {data.get("money")} <> Coins: {data.get("coin")} <> LocalIID: {data.get("localIID")}')))
+            print(Colorate.Horizontal(Colors.rainbow, Center.XCenter(f'Name: {(data.get("Name") if "Name" in data else "UNDEFINED")} <> LocalID: {data.get("localID")} <> Money: {data.get("money")} <> Coins: {data.get("coin")} <> LocalID: {data.get("localID")}')))
                 
             
         else:
