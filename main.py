15716dad88ea580ce555f7bc6c98225b06b72eaf
CPMKurdish <kura1212king@gmail.com>
2025-02-10 02:13:31 +0000

    Update main.py

--- main.py
+++ main.py
@@ -221,10 +221,10 @@ def load_player_data(cpm):
     response = cpm.get_player_data()
     if response.get('ok'):
         data = response.get('data')
-        if 'floats' in data and 'localID' in data and 'money' in data and 'coin' in data and 'car' in data:
+        if 'floats' in data and 'localID' in data and 'money' in data and 'car' in data:
         
             
-            print(Colorate.Horizontal(Colors.rainbow, Center.XCenter(f'Name: {(data.get("Name") if "Name" in data else "UNDEFINED")} <> LocalID: {data.get("localID")} <> Money: {data.get("money")} <> Coins: {data.get("coin")} <> Car: {data.get("car")}')))
+            print(Colorate.Horizontal(Colors.rainbow, Center.XCenter(f'Name: {(data.get("Name") if "Name" in data else "UNDEFINED")} <> LocalID: {data.get("localID")} <> Money: {data.get("money")} <> Car: {data.get("car")}')))
                 
             
         else:
