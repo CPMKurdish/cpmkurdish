80662c7ab44fa074597e4dbd28f5f62cf4b26b57
CPMKurdish <kura1212king@gmail.com>
2025-02-11 22:32:04 +0000

    Update cpmkurdish.py

--- cpmkurdish.py
+++ cpmkurdish.py
@@ -341,20 +341,16 @@ class CPMKurdish:
         response_decoded = response.json()
         return response_decoded.get("ok")
         
-    def unlock_equipments_female(self) -> bool:
-        payload = {"account_auth": self.auth_token}
-        params = {"key": self.access_key}
-        response = requests.post(
-            f"{BASE_URL}/unlock_equipments_female", params=params, data=payload
-        )
+    def unlock_equipaments_male(self) -> bool:
+        payload = { "account_auth": self.auth_token }
+        params = { "key": self.access_key }
+        response = requests.post(f"{BASE_URL}/unlock_equipaments_male", params=params, data=payload)
         response_decoded = response.json()
-        return response_decoded.get("ok")                    
-        
-    def unlock_equipments_male(self) -> bool:
-        payload = {"account_auth": self.auth_token}
-        params = {"key": self.access_key}
-        response = requests.post(
-            f"{BASE_URL}/unlock_equipments_male", params=params, data=payload
-        )
+        return response_decoded.get("ok")
+
+    def unlock_equipaments_female(self) -> bool:
+        payload = { "account_auth": self.auth_token }
+        params = { "key": self.access_key }
+        response = requests.post(f"{BASE_URL}/unlock_equipaments_female", params=params, data=payload)
         response_decoded = response.json()
-        return response_decoded.get("ok")                                      
+        return response_decoded.get("ok")
