1ec70e1f0ff29bb337cf492a3fbab204367eef4f
CPMKurdish <kura1212king@gmail.com>
2025-02-11 18:17:29 +0000

    Update main.py

--- main.py
+++ main.py
@@ -873,9 +873,9 @@ if __name__ == "__main__":
                 print(Colorate.Horizontal(Colors.rainbow, '[!] ENTER CAR DETALIS'))
                 car_id = IntPrompt.ask("[red][?] CAR ID[/red]")
                 print(Colorate.Horizontal(Colors.rainbow, '[!] ENTER STEERING ANGLE'))
-                custom = IntPrompt.ask("[red][?]﻿ENTER THE AMOUNT OF ANGLE YOU WANT[/red]")                
+                custom = IntPrompt.ask("[red][?]﻿ENTER THE AMOUNT OF ANGLE YOU WANT[/red]")
+                if amount > 0 and amount <= 99:
                 console.print("[red][%] HACKING CAR SPEED[/red]: ", end=None)
-                if custom > 0 and amount <= 999:
                 if cpm.max_max1(car_id, custom):
                     print(Colorate.Horizontal(Colors.rainbow, 'SUCCESSFUL'))
                     answ = Prompt.ask("[red][?] DO YOU WANT TO EXIT[/red] ?", choices=["y", "n"], default="n")
