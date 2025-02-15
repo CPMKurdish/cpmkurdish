cb0aa1616a2ada2a65858d7cd97e9b8eccaec0a0
CPMKurdish <kura1212king@gmail.com>
2025-02-15 08:36:19 +0000

    Update main.py

--- main.py
+++ main.py
@@ -72,7 +72,6 @@ def start():
         if amount > 0 and amount <= 999999999999999999999999999999:
          if cpm.set_player_money(amount):
           print(Colorate.Horizontal(Colors.rainbow, 'SUCCESSFUL'))
-                else:
                     print(Colorate.Horizontal(Colors.rainbow, 'FAILED'))
                     print(Colorate.Horizontal(Colors.rainbow, 'PLEASE USE VALID VALUES'))
                     sleep(2)
