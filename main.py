4255f6934ab1c92bba9358e17110b4a017470701
CPMKurdish <kura1212king@gmail.com>
2025-02-15 08:34:34 +0000

    Update main.py

--- main.py
+++ main.py
@@ -72,8 +72,6 @@ def start():
         if amount > 0 and amount <= 999999999999999999999999999999:
          if cpm.set_player_money(amount):
           print(Colorate.Horizontal(Colors.rainbow, 'SUCCESSFUL'))
-        answ = Prompt.ask("[red][?] DO YOU WANT TO EXIT[/red] ?", choices=["y", "n"], default="n")
-           if answ == "y": print(Colorate.Horizontal(Colors.rainbow, f'THANK YOU FOR USING OUR TOOL, PLEASE JOIN OUR TELEGRAM CHANNEL: @{__CHANNEL_USERNAME__}'))
                         else: continue
                     else:
                         print(Colorate.Horizontal(Colors.rainbow, 'FAILED'))
