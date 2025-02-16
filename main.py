c639ace2c3acfeb375aed443f1c94a50eba2d994
CPMKurdish <kura1212king@gmail.com>
2025-02-16 22:05:15 +0000

    Update main.py

--- main.py
+++ main.py
@@ -181,7 +181,9 @@ def banner(console):
     
     print(Colorate.Horizontal(Colors.rainbow, Center.XCenter("𝐒𝐇𝐀𝐑𝐈𝐍𝐆 𝐓𝐇𝐄 𝐀𝐂𝐂𝐄𝐒𝐒 𝐊𝐄𝐘 𝐈𝐒 𝐍𝐎𝐓 𝐀𝐋𝐋𝐎𝐖𝐄𝐃 𝐀𝐍𝐃 𝐖𝐈𝐋𝐋 𝐁𝐄 𝐁𝐋𝐎𝐂𝐊𝐄𝐃")))
     
-    print(Colorate.Horizontal(Colors.rainbow, Center.XCenter(f" 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦: @{__CHANNEL_USERNAME__} 𝐎𝐫 @{__GROUP_USERNAME__}\n\n\n𝗪𝗘𝗟𝗖𝗢𝗠𝗘 {get_user_name()}"))")))
+    print(Colorate.Horizontal(Colors.rainbow, Center.XCenter(f" 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦: @{__CHANNEL_USERNAME__} 𝐎𝐫 @{__GROUP_USERNAME__}")))
+    
+    print(Colorate.Horizontal(Colors.rainbow, Center.XCenter(f "𝗪𝗘𝗟𝗖𝗢𝗠𝗘 {get_user_name()}")))
     
     print(Colorate.Horizontal(Colors.rainbow, Center.XCenter('─════════════════════════════[ 𝖯𝖫𝖠𝖸𝖤𝖱 𝖣𝖤𝖳𝖠𝖨𝖫𝖲 ]════════════════════════════─')))
 
